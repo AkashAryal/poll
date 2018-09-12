f7d1a6e5cd6040873df0641c60b348e788675fd7
aryalakash <aryal.akash4414@gmail.com>
2018-09-12 09:01:50 -0500

    Linked Initial Descriptions of poll to condorchet

--- my_polls.php
+++ my_polls.php
@@ -7,23 +7,23 @@ echo '<link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 <nav class="w3-bar w3-black">
   <a href="Poll_home.html" class="w3-button w3-bar-item">Home</a>
-  <a a href="logout.php" class="w3-button w3-bar-item">Logout</a> 
+  <a a href="logout.php" class="w3-button w3-bar-item">Logout</a>
   <a href="createPoll.html" class="w3-button w3-bar-item">Create a Poll</a>
   <a href="vote.html" class="w3-button w3-bar-item">Vote</a>
   </nav>';
-  
+
 $host="localhost";
 	$dbusername = "root";
 	$dbpassword = "ILMSIWLTMCD24/7";
 	$dbname="poll";
-	
+
 	//create connection
 	$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-	
+
 	if(mysqli_connect_error()){
 		die("Connection Error (" .mysql_connect_errno(). ") " . mysql_connect_error());
 		sleep(5);
-		header('Location: user.php'); 
+		header('Location: user.php');
 	}
 	else{
 		$username=$_SESSION['user_id'];
@@ -32,39 +32,49 @@ $host="localhost";
 		$result = $conn->query($query) or die($conn->error);
 		//$result = $conn->query("select `input_date_time`, `open_time` from poll_info where poll_id='$poll_id'") or die($conn->error);
 			//$row = $result->fetch_assoc();
+      $f=0;
+      $dataHolder = array();
 		while($row = $result->fetch_assoc()){
+      $dataHolder[$f][0]=$row;
 			$ot = $row['open_time'];
 			$time = strtotime($row['input_date_time']." + $ot hour");
 			$mysqlDate = date("Y-m-d H:i:s", $time);
 			$str=$row['data']."<br />";
 			$arr=explode('|',$str);
-			echo "Poll Name: ".$arr[0]."<br />";
+    //  var_dump($arr);
+    //  $serialized=htmlspecialchars(serialize($arr));
+			echo "<a href='poll_results.php?index=$f'>Poll Name: ".$arr[0].'</a><br />';
+      $_SESSION['arr']=$dataHolder;
+    //  $_SESSION['index']=$f;
+    //  var_dump($dataHolder[0][0]);
+      //echo "<form><input type='hidden' name='arr' value='$serialized'>";
+      //echo'<input type="submit" value="Submit"></form>';
 			echo "Poll Type: ".$arr[1]."<br />";
 			echo "Poll ID: ".$row['poll_id']."<br />";
 			for($i=2; $i < count($arr); $i++){
 				$o=$i-1;
-				
+
 				if($i+1 == count($arr))
 					echo "Option $o: ".$arr[$i];
 				else
 					echo "Option $o: ".$arr[$i]."</br>";
 			}
-			
+
 			/////////---------------------------everything below is about the votes;
 			///if straepoll then this below
-			
+
 			$poll_id = $row['poll_id'];
 			$q = "select value from votes where poll_id='$poll_id'";
 			$r = $conn->query($q) or die($conn->error);
 			$ensureArrGets0FilledOnlyOnce = 0;
 			if(mysqli_num_rows($r) !=0){
-						
+
 				while($row2 = $r->fetch_assoc()){
 
 					$str2 = $row2['value'];
 					$arr2 = explode('|', $str2); //optional: 10
 					if($ensureArrGets0FilledOnlyOnce ==0){
-				
+
 						$numVotes= array_fill(0,count($arr2),0);
 						$ensureArrGets0FilledOnlyOnce--;
 					}
@@ -81,7 +91,7 @@ $host="localhost";
 				$winner = $arr2[$maxIndex]; //optiona1: 10
 				$category=substr($winner, 0, strpos($winner, ":"));
 				$votes = $numVotes[$maxIndex];
-		
+
 				echo "VOTES:<br />";
 				for($j=0; $j<count($arr2); $j++)
 				{
@@ -100,11 +110,12 @@ $host="localhost";
 				}
 			}
 			else{ //condorchet and other stuff
-				
-			}	
+
+			}
 		}
 		else
 			echo "<br>";
+      $f++;
 		}
 	}
 	$conn->close();
---
+++ poll_results.php
@@ -0,0 +1,209 @@
+<?php
+require_once 'Condorcet/__CondorcetAutoload.php' ;
+use Condorcet\Condorcet;
+  use Condorcet\Election;
+  use Condorcet\Candidate;
+  use Condorcet\CondorcetUtil;
+  use Condorcet\Vote;
+
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
+session_start();
+
+if(!isset($_SESSION['user_id'])){
+  session_destroy();
+setcookie("PHPSESSID","",time()-3600,"/");
+echo "<script>alert('Log back in!');window.location.href='login.html';</script>";
+  die();
+}
+
+  $election = new Election () ;
+
+  // Create your own candidate object
+  $candidate1 = new Candidate ('John');
+  $candidate2 = new Candidate ('Greg');
+  $candidate3 = new Candidate ('Jack');
+
+  // Register your candidates
+  $election->addCandidate($candidate1);
+  $election->addCandidate($candidate2);
+  $election->addCandidate($candidate3);
+//  $candidate4 = $election->addCandidate('Candidate 4'); //stores everything
+  //in var?
+
+  $election->addVote( array(
+                             $candidate3, // 1
+                             [$candidate1] // 2 - Tie
+                             // Last rank is optionnal. Here it's : $candidate3
+ ));
+//Session Var for poll Name
+$array= $_SESSION['arr'];
+$index = $_GET['index'];
+$row = $array[$index][0];
+
+$str=$row['data']."<br />";
+$arr=explode('|',$str);
+
+//echo "<pre>"; var_dump($array);"</pre>";
+$PollName=" ".$arr[0];
+// View :
+?><!doctype html>
+ <html>
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Poll Results</title>
+
+ 	<style>
+		.votant {
+		  display: inline-block;
+		  margin-right: 2cm;
+		}
+ 	</style>
+ </head>
+ <body>
+
+	<h1><?php echo "Poll Results: $PollName";?></h1>
+
+	<em>
+
+		<?php echo "Poll Type: ".$arr[1]." | Poll ID: ".$row['poll_id']."<br />";echo"Number of Options: ".$election->countCandidates() ;?>
+		|
+		Number of votes :
+		<?php echo $election->countVotes() ;?>
+	</em>
+
+	<h2>Candidates list :</h2>
+
+	<ul>
+	<?php
+	foreach ($election->getCandidatesList() as $candidatName)
+	{
+  //  echo $election->getCandidateObjectByName("$candidatName");
+		echo '<li>'.$candidatName.'</li>' ;
+	}
+	?>
+	</ul>
+
+  <!//originally Resgistered votes details ->
+
+
+<?php
+//echo votes h2
+/*
+	foreach ($election->getVotesList() as $vote)
+	{
+	//	echo '<div class="votant">';
+		echo '<strong style="color:green;">'.implode(' / ',$vote->getTags()).'</strong>';
+		echo "<ul>";
+		foreach ($vote as $rank => $value)
+		{
+			if ($rank == 'tag') {continue ;}
+		?>
+
+			<li><?php echo implode(',',$value);?></li>
+
+		<?php
+		}
+		echo '</ul>' ;
+	}
+  */
+?>
+<hr style="clear:both;">
+
+	<h2>Winner by <a target="blank" href="http://en.wikipedia.org/wiki/Condorcet_method">natural Condorcet</a> :</h2>
+
+	<strong style="color:green;">
+		<?php
+		if ( !is_null($election->getWinner()) )
+			{ echo $election->getWinner() ;}
+		else
+			{ echo '<span style="color:red;">The votes of this group do not allow natural Condorcet winner because of <a href="http://fr.wikipedia.org/wiki/Paradoxe_de_Condorcet" target="_blank">Condorcet paradox</a>.</span>'; }
+		?>
+		<br>
+		<em style="color:green;">computed in <?php echo $election->getLastTimer() ; ?> second(s).</em>	</strong>
+
+	<h2>Loser by <a target="blank" href="http://en.wikipedia.org/wiki/Condorcet_method">natural Condorcet</a> :</h2>
+
+	<strong style="color:green;">
+		<?php
+		if ( !is_null($election->getLoser()) )
+			{ echo $election->getLoser() ;}
+		else
+			{ echo '<span style="color:red;">The votes of this group do not allow natural Condorcet loser because of <a href="http://fr.wikipedia.org/wiki/Paradoxe_de_Condorcet" target="_blank">Condorcet paradox</a>.</span>'; }
+		?>
+		<br>
+		<em style="color:green;">computed in <?php echo $election->getLastTimer() ; ?> second(s).</em>	</strong>
+	</strong>
+
+<br><br><hr>
+
+<?php
+	foreach (Condorcet::getAuthMethods() as $method)
+	{ ?>
+
+		<h2>Ranking by <?php echo $method ?>:</h2>
+
+		<?php
+			$result = $election->getResult($method) ;
+			$lastTimer = $election->getLastTimer() ;
+			if ( $method === 'Kemeny–Young' && !empty($result->getWarning(\Condorcet\Algo\Methods\KemenyYoung::CONFLICT_WARNING_CODE)) )
+			{
+				$kemeny_conflicts = explode( ';', $result->getWarning(\Condorcet\Algo\Methods\KemenyYoung::CONFLICT_WARNING_CODE)[0]['msg'] ) ;
+				echo '<strong style="color:red;">Arbitrary results: Kemeny-Young has '.$kemeny_conflicts[0].' possible solutions at score '.$kemeny_conflicts[1].'</strong>' ;
+			}
+		 ?>
+
+		<pre>
+		<?php
+    $temp = print_r( CondorcetUtil::format($result), true );
+    $print = substr(substr($temp,0,-3),7);
+    echo "$print";
+    ?>
+		</pre>
+
+		<em style="color:green;">computed in <?php echo $lastTimer ; ?> second(s).</em>
+
+	<?php }
+?>
+<br><br><hr><br>
+<strong style="color:green;">Total computed in <?php echo $election->getGlobalTimer() ; ?> second(s).</strong>
+<br>
+<?php var_dump($election->getTimerManager()->getHistory()); ?>
+<br><br><hr>
+
+<h2>Computing statistics :</h2>
+
+	<h3>Pairwise :</h3>
+
+	<pre>
+	<?php var_dump( CondorcetUtil::format($election->getPairwise()) ); ?>
+	</pre>
+
+	<?php
+	foreach (Condorcet::getAuthMethods() as $method)
+	{ ?>
+		<h3>Stats for <?php echo $method ?>:</h3>
+
+		<pre>
+		<?php var_dump( CondorcetUtil::format($election->getResult($method)->getStats()) ); ?>
+		</pre>
+
+	<?php } ?>
+
+ <br><br><hr>
+
+<h2>Debug Data :</h2>
+
+ <h4>Defaut method (not used explicitly before) :</h4>
+
+ <pre>
+<?php var_dump( CondorcetUtil::format(Condorcet::getDefaultMethod()) ); ?>
+ </pre>
+
+<!-- <h4>CondorcetUtil::format (for debug only) :</h4>
+ <pre>
+<?php // CondorcetUtil::format($election); ?>
+ </pre> -->
+
+ </body>
+ </html>
